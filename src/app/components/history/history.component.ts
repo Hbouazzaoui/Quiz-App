5e720e22dfa87c6b53ceb66b5478f7ca30b13821
Hbouazzaoui <bouazzaouiihamouda@gmail.com>
2025-04-18 12:31:53 +0100

    create component history

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './history.component.html',
   styleUrl: './history.component.scss'
 })
