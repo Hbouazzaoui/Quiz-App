497eb082533eb5129e1d9e9540bd02bb81f30cab
Hbouazzaoui <bouazzaouiihamouda@gmail.com>
2025-04-18 12:32:11 +0100

    create component home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,25 @@
+// src/app/components/home/home.component.ts
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NavbarComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  difficulty = 'easy';
+  questionCount = '10';
 
+  constructor(private router: Router) {}
+
+  startQuiz() {
+    this.router.navigate(['/quiz'], {
+      queryParams: { difficulty: this.difficulty, amount: this.questionCount },
+    });
+  }
 }
