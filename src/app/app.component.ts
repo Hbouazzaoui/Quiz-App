f30fb8b120ff5d1ac9fe82c4b1cb1b5efb5441ac
Hbouazzaoui <bouazzaouiihamouda@gmail.com>
2025-04-19 21:50:16 +0100

    Add FooterComponent to Quiz App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'quiz-app'; 
+}
